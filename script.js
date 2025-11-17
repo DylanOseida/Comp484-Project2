b2a0af32c53e4214a16388e3f60be1f86ce9a04c
DylanOseida <dylan.oseida.103@my.csun.edu>
2025-11-17 14:33:46 -0800

    version 1 update

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 $(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
-    
+
     // Called function to update the name, happiness, and weight of our pet in our HTML
     checkAndUpdatePetInfoInHtml();
-  
+
     // When each button is clicked, it will "call" function for that button (functions are below)
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
-
-    
   })
   
   // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-  var pet_info = {name:"Jeff the Land Shark", weight:"??", happiness:"??"};
+  var pet_info = {name:"Jeff the Land Shark", weight:1, happiness:1};
 
   function clickedTreatButton() {
+    
     // Increase pet happiness
+    pet_info.happiness++;
+
     // Increase pet weight
+    pet_info.weight++;
+    $('.pet-image').animate({width: '+=5', height: '+=5'}, 100);
+
     checkAndUpdatePetInfoInHtml();
   }
   
   function clickedPlayButton() {
+
     // Increase pet happiness
+    pet_info.happiness++;
+
     // Decrease pet weight
+    pet_info.weight--;
+    $('.pet-image').animate({width: '-=5', height: '-=5'}, 100);
+
     checkAndUpdatePetInfoInHtml();
   }
   
   function clickedExerciseButton() {
+
     // Decrease pet happiness
+    if (pet_info.happiness <= 1){
+     pet_info.happiness--;
+    }
+    else{
+      pet_info.happiness -= 2;
+    }
+
     // Decrease pet weight
+    pet_info.weight--;
+    $('.pet-image').animate({width: '-=5', height: '-=5'}, 100);
+
     checkAndUpdatePetInfoInHtml();
   }
 
@@ -38,12 +59,50 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
   }
   
   function checkWeightAndHappinessBeforeUpdating() {
+
+    /***********************************************************
+    *                                                          *
+    *                .prop() method used here                  *
+    *                                                          *
+    ***********************************************************/
+
     // Add conditional so if weight is lower than zero.
+    if(pet_info.weight == 0 || pet_info.happiness == 0){
+      $('.play-button, .exercise-button').prop('disabled', true) //disable play and exercise button
+    }
+    else{
+      $('.play-button, .exercise-button').prop('disabled', false) //enable play and exercise button
+    }
+
+    var mood;
+
+    if (pet_info.happiness == 0 && pet_info.weight == 0){
+      mood = 'Hangry'
+    }
+    else if (pet_info.happiness > 0 && pet_info.weight == 0){
+      mood = 'Hungry'
+    }
+    else if (pet_info.happiness > pet_info.weight || pet_info.happiness == pet_info.weight){
+      mood = 'Happy'
+    }
+    else if(pet_info.happiness < pet_info.weight){
+      mood = 'Fat'
+    }
+
+    /***********************************************************
+    *                                                          *
+    *                .data() method used here                  *
+    *                                                          *
+    ***********************************************************/
+
+    // Attach mood variable to pet-image and store mood using .data()
+    $('.pet-image').data('mood', mood);
   }
-  
+
   // Updates your HTML with the current values in your pet_info object
   function updatePetInfoInHtml() {
     $('.name').text(pet_info['name']);
     $('.weight').text(pet_info['weight']);
     $('.happiness').text(pet_info['happiness']);
+    $('.mood').text($('.pet-image').data('mood'));
   }
